7752ad4d81ef35e3c215ed0c2f12346373728cc1
Christian <Christian@USER-PC>
2019-11-12 00:51:27 -0500

    Actualizacion Vistas de Ticket

--- GeekRepairs/src/main/java/com/geek/model/Ticket.java
+++ GeekRepairs/src/main/java/com/geek/model/Ticket.java
@@ -10,12 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.geek.dateAudit.DateAudit;
-
 
 @Entity
 @Table(name = "tickets")
@@ -29,12 +30,13 @@ public class Ticket extends DateAudit {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private Client client;
-	
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
+
+	// @DateTimeFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name = "dateAttention", nullable = false)
 	private Date dateAttention;
-	
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name = "dateService")
 	private Date dateService;
 
@@ -48,13 +50,10 @@ public class Ticket extends DateAudit {
 	@NotEmpty(message = "Please enter a address.")
 	private String address;
 
-
 	public Ticket() {
 		this.setCreatedAt(new Date());
 		this.setUpdatedAt(new Date());
 	}
-
-	
 
 	public Ticket(Long id, Client client, Date dateAttention, Date dateService,
 			@NotEmpty(message = "Please enter a typeservice") String typeService,
@@ -70,79 +69,60 @@ public class Ticket extends DateAudit {
 		this.address = address;
 	}
 
-
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public Client getClient() {
 		return client;
 	}
-
 
 	public void setClient(Client client) {
 		this.client = client;
 	}
 
-
 	public Date getDateAttention() {
 		return dateAttention;
 	}
-
 
 	public void setDateAttention(Date dateAttention) {
 		this.dateAttention = dateAttention;
 	}
 
-
 	public Date getDateService() {
 		return dateService;
 	}
-
 
 	public void setDateService(Date dateService) {
 		this.dateService = dateService;
 	}
 
-
 	public String getTypeService() {
 		return typeService;
 	}
-
 
 	public void setTypeService(String typeService) {
 		this.typeService = typeService;
 	}
 
-
 	public String getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
-
 	public String getAddress() {
 		return address;
 	}
 
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
-	
 
-	
-	
 }
