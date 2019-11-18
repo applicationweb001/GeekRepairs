855b7e0db7c41ffbbd1505c1caa571af46e485bd
kevin <kevin@192.168.1.60>
2019-11-18 00:22:30 -0500

    HU productos-arreglo de bug

--- GeekRepairs/src/main/java/com/geek/controller/ProductController.java
+++ GeekRepairs/src/main/java/com/geek/controller/ProductController.java
@@ -66,12 +66,12 @@ public class ProductController {
 	@Secured({"ROLE_ADMIN"})
 	@GetMapping("/new")
 	public String newProduct(Model model) {
-
+		List<Category> categories = categoryService.getAll(); //se agrego esto
+		model.addAttribute("categories",categories); // y esto
 		// in case of redirection model will contain article
 		if (!model.containsAttribute("product")) {
 			model.addAttribute("product", new Product());
-			List<Category>categories  = categoryService.getAll(); //se agrego esto
-			model.addAttribute("categories",categories); // y esto
+			
 		}
 		return PRODUCT_ADD_FORM_VIEW;
 	}
@@ -86,8 +86,8 @@ public class ProductController {
 			// After the redirect: flash attributes pass attributes to the model
 			attr.addFlashAttribute("org.springframework.validation.BindingResult.product", result);
 			attr.addFlashAttribute("product", product);
-			List<Category>categories  = categoryService.getAll(); //se agrego esto
-			attr.addFlashAttribute("categories",categories); // y esto
+			List<Category> categories = categoryService.getAll(); //se agrego esto
+			model.addAttribute("categories",categories); // y esto
 			
 
 			return "redirect:/products/new";
@@ -105,10 +105,11 @@ public class ProductController {
 		 * in case of redirection from '/article/{id}/update' model will contain article
 		 * with field values
 		 */
+		List<Category> categories = categoryService.getAll(); //se agrego esto
+		model.addAttribute("categories",categories); // y esto
 		if (!model.containsAttribute("product")) {
 			model.addAttribute("product", productService.findById(productId));
-			List<Category> categories = categoryService.getAll(); //se agrego esto
-			model.addAttribute("categories",categories); // y esto
+			
 		}
 		return PRODUCT_EDIT_FORM_VIEW;
 	}
@@ -124,8 +125,8 @@ public class ProductController {
 			/// After the redirect: flash attributes pass attributes to the model
 			attr.addFlashAttribute("org.springframework.validation.BindingResult.product", result);
 			attr.addFlashAttribute("product", productDetails);
-			List<Category>categories  = categoryService.getAll(); //se agrego esto
-			attr.addFlashAttribute("categories",categories); // y esto
+			List<Category> categories = categoryService.getAll(); //se agrego esto
+			model.addAttribute("categories",categories); // y esto
 			
 
 			return "redirect:/products/" + productDetails.getId() + "/edit";
